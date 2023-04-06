065618c591bd3dd894f0d1f00d01f363e10d6530
Malurix <122803018+Malurix@users.noreply.github.com>
2023-04-06 21:21:51 +0200

    Add files via upload

---
+++ Spam.py
@@ -0,0 +1,25 @@
+import pyautogui as auto
+from time import sleep
+pocet = 0
+while True:
+  zprava = "ZAHRAJ SI MINECRAFT! :D"
+  auto.write(zprava)
+  auto.press("enter")
+  pocet = pocet + 1
+  print("Napsal jsem jiz " + str(pocet) + "x zpravu: " + zprava)
+  auto.write(zprava)
+  auto.press("enter")
+  pocet = pocet + 1
+  print("Napsal jsem jiz " + str(pocet) + "x zpravu: " + zprava)
+  auto.write(zprava)
+  auto.press("enter")
+  pocet = pocet + 1
+  print("Napsal jsem jiz " + str(pocet) + "x zpravu: " + zprava)
+  auto.write(zprava)
+  auto.press("enter")
+  pocet = pocet + 1
+  print("Napsal jsem jiz " + str(pocet) + "x zpravu: " + zprava)
+  auto.write(zprava)
+  auto.press("enter")
+  pocet = pocet + 1
+  print("Napsal jsem jiz " + str(pocet) + "x zpravu: " + zprava)
